d736f4832262ba5aecdcdd120d7a51a7e38da43a
Masum Raza <56122183+masumrazait@users.noreply.github.com>
2023-06-15 15:04:28 +0530

    Created Automation FrameWork End To End Testing For Para Bank WebUI

---
+++ src/main/java/com/parabank/dbTesting/DbTesting.java
@@ -0,0 +1,18 @@
+package com.parabank.dbTesting;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class DbTesting {
+
+	public static void main(String[] args) throws SQLException {
+		// TODO Auto-generated method stub
+
+		DbManager.setMsqlDbConnection();
+		List<String>query=DbManager.getMySqlQuery("SELECT * FROM world.country;");
+		for(int i=0;i<query.size();i++) {
+			System.out.println(query.get(i));
+		}
+	}
+
+}
---
+++ src/main/java/com/parabank/pageObjects/Test.java
@@ -0,0 +1,10 @@
+package com.parabank.pageObjects;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
