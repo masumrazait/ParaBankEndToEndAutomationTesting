a882e3bf6ef8cf57e92a06170ae51cef00fb43c4
Masum Raza <56122183+masumrazait@users.noreply.github.com>
2023-06-21 05:17:20 +0530

    Added new TestCase for data validation from table to view

---
+++ src/main/java/com/parabank/dbTesting/DataValidationTableToView.java
@@ -0,0 +1,106 @@
+package com.parabank.dbTesting;
+
+import java.awt.*;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DataValidationTableToView {
+
+	public static void main(String[] args) throws SQLException, IOException {
+		DbManager.setMsqlDbConnection();
+
+		// Get data from the table
+		List<Map<String, Object>> tableData = getTableData("select * from mypersonaldata.population_data");
+		System.out.println("************************** Data validation from Table: *************************"+"\n");
+		printData(tableData);
+		System.out.println("\n");
+
+		// Get data from the view
+		List<Map<String, Object>> viewData = getTableData("select * from mypersonaldata.population_data_v");
+		System.out.println("************************** Data validation from View: ******************************"+"\n");
+		printData(viewData);
+		System.out.println("\n");
+
+		boolean isDataEqual = tableData.equals(viewData);
+
+		if (isDataEqual) {
+			System.out.println("\n" + "\033[32m" + "***** Pass ***** The data from Table and View is the same."
+					+ "\033[0m" + "\n");
+			saveDataToCSV(tableData, true);
+		} else {
+			System.out.println("\033[31m" + "***** Fail ***** The data from Table and View is different." + "\033[0m");
+			saveDataToCSV(tableData, false);
+		}
+	}
+
+	private static List<Map<String, Object>> getTableData(String query) throws SQLException {
+		List<Map<String, Object>> data = new ArrayList<Map<String, Object>>();
+		ResultSet resultSet = DbManager.executeQuery(query);
+
+		ResultSetMetaData metaData = resultSet.getMetaData();
+		int columnCount = metaData.getColumnCount();
+
+		while (resultSet.next()) {
+			Map<String, Object> row = new HashMap<String, Object>();
+			for (int i = 1; i <= columnCount; i++) {
+				String columnName = metaData.getColumnName(i);
+				Object value = resultSet.getObject(i);
+				row.put(columnName, value);
+			}
+			data.add(row);
+		}
+
+		return data;
+	}
+
+	private static void printData(List<Map<String, Object>> data) {
+		for (Map<String, Object> row : data) {
+			for (Map.Entry<String, Object> entry : row.entrySet()) {
+				String columnName = entry.getKey();
+				Object value = entry.getValue();
+				System.out.print(columnName + "[" + value + "], ");
+			}
+			System.out.println();
+		}
+	}
+
+	private static void saveDataToCSV(List<Map<String, Object>> data, boolean isDataEqual) throws IOException {
+		String filePath = "D:\\My_Project\\Testing_Project\\ParaBankEndToEndAutomationTesting\\DataValidationReport\\data.csv";
+		FileWriter csvWriter = new FileWriter(filePath);
+
+		// Write column headers
+		if (!data.isEmpty()) {
+			Map<String, Object> firstRow = data.get(0);
+			for (String columnName : firstRow.keySet()) {
+				csvWriter.append(columnName);
+				csvWriter.append(",");
+			}
+			csvWriter.append("Status"); // Add Status column header
+			csvWriter.append("\n");
+		}
+
+		// Write data rows
+		for (Map<String, Object> row : data) {
+			for (Object value : row.values()) {
+				if (value != null) {
+					csvWriter.append(value.toString());
+				}
+				csvWriter.append(",");
+			}
+			// Write Status value as Pass (green) or Fail (red)
+			csvWriter.append(isDataEqual ? "Passed" : "Failed");
+			csvWriter.append("\n");
+		}
+
+		csvWriter.flush();
+		csvWriter.close();
+		System.out.println("Data saved to CSV file: " + filePath);
+	}
+}
--- src/main/java/com/parabank/dbTesting/DbManager.java
+++ src/main/java/com/parabank/dbTesting/DbManager.java
@@ -11,48 +11,63 @@ import java.util.List;
 import com.parabank.utilities.TestConfig;
 
 public class DbManager {
-	private static Connection con =null;
-	private static Connection conn =null;
-	
+	private static Connection con = null;
+	private static Connection conn = null;
+
 	public static void setDbConnection() {
 		try {
 			Class.forName(TestConfig.driver);
-			con = DriverManager.getConnection(TestConfig.dbConnectionUrl,TestConfig.dbUserName,TestConfig.dbPassword);
-			
-			if(!con.isClosed())
-				System.out.println("SuccessFully Connected to SQL Server");
-		}catch (Exception e) {
-			// TODO: handle exception
-			System.out.println("Exception : " + e.getMessage());
+			con = DriverManager.getConnection(TestConfig.dbConnectionUrl, TestConfig.dbUserName, TestConfig.dbPassword);
+
+			if (!con.isClosed())
+				System.out.println("Successfully connected to SQL Server");
+		} catch (Exception e) {
+			System.out.println("Exception: " + e.getMessage());
 		}
 	}
-	
+
 	public static void setMsqlDbConnection() {
 		try {
 			Class.forName(TestConfig.mysqldriver).newInstance();
-			conn=DriverManager.getConnection(TestConfig.mysqlurl,TestConfig.mysqluserName,TestConfig.mysqlpassword);
-			
-			if(!conn.isClosed())
-				System.out.println("Successfull connected to the MySQL Server");
-		}catch (Exception e) {
-			// TODO: handle exception
-			System.out.println("Cannot Connected to the Database Server");
+			conn = DriverManager.getConnection(TestConfig.mysqlurl, TestConfig.mysqluserName, TestConfig.mysqlpassword);
+
+			if (!conn.isClosed())
+				System.out.println("Successfully connected to the MySQL WorkBench" + "\n");
+		} catch (Exception e) {
+			System.out.println("Cannot connect to the Database Server");
 		}
 	}
-	
-	
-	public static List<String>getMySqlQuery(String query)throws SQLException{
-		Statement St =conn.createStatement();
-		ResultSet rs=St.executeQuery(query);
-		List<String>values1=new ArrayList<String>();
-		while(rs.next()) {
+
+	public static List<String> getMySqlQueryTable(String query) throws SQLException {
+		Statement st = conn.createStatement();
+		ResultSet rs = st.executeQuery(query);
+		List<String> values1 = new ArrayList<String>();
+		while (rs.next()) {
 			values1.add(rs.getString(1));
 			values1.add(rs.getString(2));
 			values1.add(rs.getString(3));
 			values1.add(rs.getString(4));
 		}
-	
+
 		return values1;
 	}
 
+	public static List<String> getMySqlQueryFromView(String query) throws SQLException {
+		Statement st = conn.createStatement();
+		ResultSet rs = st.executeQuery(query);
+		List<String> values2 = new ArrayList<String>();
+		while (rs.next()) {
+			values2.add(rs.getString(1));
+			values2.add(rs.getString(2));
+			values2.add(rs.getString(3));
+			values2.add(rs.getString(4));
+		}
+
+		return values2;
+	}
+
+	public static ResultSet executeQuery(String query) throws SQLException {
+		Statement st = conn.createStatement();
+		return st.executeQuery(query);
+	}
 }
